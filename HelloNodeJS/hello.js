e703eda22a43e29d73f867d9284f98c8ff23faab
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-03-29 19:27:55 +0500

    hello nodejs

---
+++ HelloNodeJS/hello.js
@@ -0,0 +1,6 @@
+const math = require("./math");
+
+console.log("Hey there! I'm a Node.js script!");
+
+console.log("math FUnction is", math.addFn(1, 2));
+console.log("math FUnction is", math.subFn(1, 2));
