89d55ce815c955cf1767ea9cf5c3c355810920a8
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-03-30 01:19:48 +0500

    connect mongodb

--- Project-01/index.js
+++ Project-01/index.js
@@ -4,38 +4,72 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 // MIDDLEWARES
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    console.log("Hello from middleware 1");
-    next();
-})
+  console.log("Hello from middleware 1");
+  next();
+});
 
 app.use((req, res, next) => {
-    console.log("Hello from middleware 2");
-    next();
+  console.log("Hello from middleware 2");
+  next();
 });
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/Youtube-DB-1", {})
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Schema mongoose
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    jobTitle: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// mongoose model
+
+const User = mongoose.model("User", userSchema);
 
 // ROUTES
 
-app.get("/api/users", (req, res) => {
-  return res.json(users);
+app.get("/api/users", async (req, res) => {
+  const allDBUsers = await User.find();
+  return res.json(allDBUsers);
 });
 
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+  const allDBUsers = await User.find();
   const html = `
     <ul>
-    ${users
+    ${allDBUsers
       .map(
-        (user) => `<li>${user.id}</li>
-    <li>${user.first_name}</li>
-    <li>${user.last_name}</li>
-    <li>${user.gender}</li>
+        (user) => `
+    <li>${user.firstName}</li>
     <li>${user.email}</li>
-    <li>${user.job_title}</li>
     `
       )
       .join("")}
@@ -47,46 +81,51 @@ app.get("/users", (req, res) => {
 // Get user with id
 app
   .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = req.find((user) => user.id === id);
+  .get(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    // const user = req.find((user) => user.id === id);
     if (!user) {
-      return res.status(404).json({ status: "fail", message: "User not found" });
+      return res
+        .status(404)
+        .json({ status: "fail", message: "User not found" });
     }
     return res.json(user);
   })
-  .patch((req, res) => {
+  .patch(async (req, res) => {
     // TODO : edit user with id
-    const id = Number(req.params.id);
-    const body = req.body;
-    const user = users.find((user) => user.id === id);
-    const index = users.indexOf(user);
-    users[index] = { ...user, ...body };
-    fs.writeFileSync("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-      return res.json({ status: "success" });
-    });
+    await User.findByIdAndUpdate(req.params.id, { lastName: "Changed" });
+    return res.json({ status: "success" });
   })
-  .delete((req, res) => {
+  .delete(async (req, res) => {
     // TODO : delete user with id
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    const index = users.indexOf(user);
-    users.splice(index, 1);
-    fs.writeFileSync("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-      return res.json({ status: "success" });
-    });
+    await User.findByIdAndDelete(req.params.id);
+    return res.json({ status: "success" });
   });
 
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
   // TODO: create new user
   const body = req.body;
-  if(!body || !body.first_name || !body.last_name || body.email || body.job_title || body.gender){
-    return res.status(400).json({status: "fail", message: "Please provide all the details"});
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.job_title ||
+    !body.gender
+  ) {
+    return res
+      .status(400)
+      .json({ status: "fail", message: "Please provide all the details" });
   }
-  users.push({...body, id: users.length + 1});
-  fs.writeFileSync("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.status(201).json({ status: "success", id: id.length});
+  const result = await users.create({
+    firstName: body.firstName,
+    lastName: body.lastName,
+    gender: body.gender,
+    jobTitle: body.jobTitle,
+    email: body.email,
   });
+  console.log("result", result);
+  return res.status(201).json({ status: "success", data: result });
 });
 
 app.listen(PORT, () => console.log(`Server is running on port :${PORT}`));
